f6209f2ef7bd6214decb4a02694d48e35b42a51f
Umar-25 <132724909+Umar-25@users.noreply.github.com>
2024-12-05 19:54:47 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+//number=> 2^53
+//bigint=> for larger numbers
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+//objects
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
